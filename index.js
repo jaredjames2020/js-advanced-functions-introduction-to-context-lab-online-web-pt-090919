c0dde5d33f241d8116d5c556e8e782fbe7732a62
Jared S <jaredjames2020@gmail.com>
2020-04-27 22:47:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Your code here
+const createEmployeeRecord = function(payroll) {
+    payroll.firstName = payroll[0];
+    payroll.familyName = payroll[1];
+    payroll.title = payroll[2];
+    payroll.payPerHour = payroll[3];
+    payroll.timeInEvents = [];
+    payroll.timeOutEvents = [];
+    return payroll;
+};
+
+const createEmployeeRecords = function(records){
+  return records.map(rec => createEmployeeRecord(rec));
+};
+
+function createTimeInEvent(){
+  let time = Time.now
+  
+}
